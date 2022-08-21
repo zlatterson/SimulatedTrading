5a4190bb571fd0ffdc3cb5aa5a7258bf88481491
Andrew <zlatanpatterson@gmail.com>
2022-08-21 19:44:12 +0100

    added simplified and pythonic planning

---
+++ backend/controllers/user_controller.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+from models.user import User
+
+# user_blueprint = Blueprint("user", __name__)
+# @users_blueprint.route("/users/<id>")
+# def show_users(id):
+#     found_user = user_repository.select(id)
--- backend/models/stock_wrapper.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-
-class StockWrapper:
-    def __init__(self,stock):
-        self.stock = stock
-        self.last_access = datetime.now()
