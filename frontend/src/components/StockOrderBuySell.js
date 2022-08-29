cf8fd31bfe408782ef774fd8c533431227ce5540
Andrew <zlatanpatterson@gmail.com>
2022-08-29 18:06:05 +0100

    added logic to limit max quantity input

--- frontend/src/components/StockOrderBuySell.js
+++ frontend/src/components/StockOrderBuySell.js
@@ -3,11 +3,16 @@ import React from 'react';
 const StockOrderBuySell = ({user, stock, quantityInput, setQuantityInput, setSentBuySellOrder }) => {
 
     const handleSearchInput = (e) => {
-        setQuantityInput(e.target.value)
+        let { value, min, max } = e.target;
+        value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+        setQuantityInput(value)
       }
       const newSubmit = (e) => {
         e.preventDefault();
-        setSentBuySellOrder(quantityInput)
+        let jsonArray = [];
+        let jsonObject = { name : 'abc',quantity : quantityInput, gender : 'Male'};
+        jsonArray.push(jsonObject);
+        setSentBuySellOrder(jsonArray)
       }
 
     return (
@@ -15,11 +20,11 @@ const StockOrderBuySell = ({user, stock, quantityInput, setQuantityInput, setSen
         {user !== null && stock !== null ? 
             <div>
                 <h5>Current price is: ${stock._current_price.toFixed(2)}</h5>
-                <h5>Available Balance: ${user.money.toFixed(2)}</h5>
+                <h5>Available Balance: ${(user.money.toFixed(2))}</h5>
                 <h5>Estimated Cost: ${(quantityInput * stock._current_price).toFixed(2)}</h5>
                 <h5>{quantityInput * stock._current_price > user.money ? "Not Enough money" : "Confirm Order"}</h5>
                 <form onSubmit={newSubmit}>
-                <input type="number" onChange={handleSearchInput} placeholder="Quantity..."></input>
+                <input type="number" min="1" max={((user.money / stock._current_price).toFixed(0)) - 1} onChange={handleSearchInput} placeholder="Quantity..."></input>
                 <input type="submit" value="Order" />
                 </form>
             </div>
